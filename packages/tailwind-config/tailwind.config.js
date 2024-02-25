7c73ccb4297682c9f35211440e1b111568e73045
kyusikko <kokyusik91@naver.com>
2024-02-25 16:08:35 +0900

    ➕ setting common tailwind config packages

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../../packages/ui/**/*.{js,ts,jsx,tsx}', // Add the ui package
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
