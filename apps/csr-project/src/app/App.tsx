23c58250e92ae7df469e5201cdd9f0a275b5dc05
kyusikko <kokyusik91@naver.com>
2024-03-09 18:06:12 +0900

    🔧  change directories for using fsd

--- apps/csr-project/src/app/App.tsx
+++ apps/csr-project/src/app/App.tsx
@@ -1,9 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 
-import router from '@router/index';
-
 import './App.css';
+import router from './router';
 
 // 최상단 컴포넌트
 function App() {
--- apps/csr-project/src/main.tsx
+++ apps/csr-project/src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import '@repo/tailwind-config/style.css';
 import ReactDOM from 'react-dom/client';
-
-import App from './App.tsx';
+import App from '@app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
