ecf1e967aed8bfcf4dfca98d0dce15d0fdd61ace
kyusikko <kokyusik91@naver.com>
2024-03-10 16:01:23 +0900

    🧪 implement tailwind animation in tailwind config file

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -10,4 +10,17 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
+  },
 };
