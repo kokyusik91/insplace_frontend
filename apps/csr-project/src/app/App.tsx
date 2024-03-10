af3363509d7e311ac795e905d459f8a74e6bc792
kyusikko <kokyusik91@naver.com>
2024-03-10 12:03:24 +0900

    ⚰️ Delete unused App.css file

--- apps/csr-project/src/app/App.tsx
+++ apps/csr-project/src/app/App.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 
-import './App.css';
 import router from './router';
 
 // 최상단 컴포넌트
