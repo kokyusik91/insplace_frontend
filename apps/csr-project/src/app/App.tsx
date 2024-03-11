ccd23d7569dc84854a4b85338b7c83027fcb862a
kyusikko <kokyusik91@naver.com>
2024-03-11 22:44:41 +0900

    📦 addon tanstack devtool

--- apps/csr-project/src/app/App.tsx
+++ apps/csr-project/src/app/App.tsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { RouterProvider } from 'react-router-dom';
 
 import router from './router';
@@ -10,6 +12,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
